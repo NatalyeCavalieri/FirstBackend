1fae2d7616399f52bb4775ece6566da101b402f8
Natalye <cavalieridev@gmail.com>
2023-12-11 19:38:32 +0100

    registering note

--- src/database/knex/migrations/20231211161034_createLinks.js
+++ src/database/knex/migrations/20231211161034_createLinks.js
@@ -1,10 +1,8 @@
-const { on } = require("nodemon");
-
-exports.up = (knex) =>
-  knex.schema.createTable("links", (table) => {
+exports.up = (knex) => knex.schema.createTable("links", table => {
     table.increments("id")
     table.text("url").notNullable();
-    table.integer("notes_id").references("id").inTable("notes").onDelete("CASCADE");
+    
+    table.integer("note_id").references("id").inTable("notes").onDelete("CASCADE");
     table.timestamp("created_at").default(knex.fn.now())
   })
 
