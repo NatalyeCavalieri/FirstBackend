879a4a737fa26604758be4abae10731909b9e257
Natalye <cavalieridev@gmail.com>
2023-12-12 11:28:27 +0100

    delete note

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -43,6 +43,12 @@ class NotesController {
       links
     })
   }
+
+  async delete (request, response){
+    const { id } = request.params
+    await knex("notes").where({ id }).delete()
+    return response.json()
+  }
 }
 
 module.exports = NotesController
