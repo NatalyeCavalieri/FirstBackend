c7bebfdeec205d2e211b693104b6538d1844a8bf
Natalye <cavalieridev@gmail.com>
2024-01-07 07:21:27 +0100

    added avatar file in TMP folder

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,21 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/usersController");
-
-const ensureAuthenticated = require("../middleware/ensureAuthenticated")
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload")
 
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER)
 
 
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create)
 usersRoutes.put("/", ensureAuthenticated, usersController.update)
-
-
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) =>{
+  console.log(request.file.filename)
+  response.json()
+})
 
 module.exports = usersRoutes;
